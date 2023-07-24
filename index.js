4ba1e34b081ea4b920fa771eb0c8ae5316b625c3
Josielen Rubin <josirubinc@hotmail.com>
2023-07-24 18:34:42 -0300

    Configurações iniciais do projeto

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-module.exports = () => {
-  // ...
-};
+const fs = require('fs')
+const path = require('path')
+const axios = require('axios')
+
+// fs.readFile(path.join (__dirname, '/src/files', 'markdown.md'), 'utf8', (err, data) =>{
+//   if (err){
+//     console.log('Erro: ', err)
+//   }console.log(data)
+// })
+
+// function mdLinks (){
+  // 
+// }
+
+// module.exports = () => {
+//   mdLinks
+// };
